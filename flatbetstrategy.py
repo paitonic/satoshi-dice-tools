2648cfc78602beeeff91ca3736b512cf2eb2d29c
tranq <edoomik@gmail.com>
2015-03-21 14:54:27 +0200

    project rename; turned satoshidicetools into a python package

--- flatbetstrategy.py
+++
@@ -1,8 +0,0 @@
-import dicesimulator
-
-class Flatbet(dicesimulator.DiceSimulator):
-    def strategy(self, previous_bet):
-        pass
-
-flatbet = Flatbet(balance=0.001, bet_amount=0.0001, less_then=45000)
-flatbet.run()
--- randomizerstrategy.py
+++
@@ -1,9 +0,0 @@
-import dicesimulator
-import random
-
-class Randomizer(dicesimulator.DiceSimulator):
-    def strategy(self, previous_bet):
-        self.less_then = random.randint(1, 64225)
-
-randomizer = Randomizer(balance=0.0001, bet_amount=0.000001, less_then=49152)
-randomizer.run()
